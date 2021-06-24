f18df0efa2fdd93e7f3466ef3bea9d35c01b1d1a
FelipePDS <felipepdasilva66@gmail.com>
2021-06-24 18:59:26 -0300

    change MenuTopHome to MenuTop

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { ProfileContext } from '../../context/ProfileContext';
+import { UpdateProfileList } from '../../utils/profileUtil';
 
 import { Container } from './styles';
 
-import MenuTopHome from '../../components/MenuTopHome/';
+import MenuTop from '../../components/MenuTop/';
 import ProfileControl from '../../components/ProfileControl/';
 
 const Home: React.FC = () => {
+  const { profileList } = useContext(ProfileContext);
+
+  UpdateProfileList(profileList);
+
   return (
     <Container>
-      <MenuTopHome />
+      <MenuTop />
 
       <ProfileControl />
     </Container>
