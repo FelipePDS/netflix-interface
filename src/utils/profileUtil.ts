f5154994eaa337e3dec951486e59aca68f5465c3
FelipePDS <felipepdasilva66@gmail.com>
2021-06-24 19:00:10 -0300

    add UpdateProfileList

---
+++ src/utils/profileUtil.ts
@@ -0,0 +1,26 @@
+import { useEffect, useContext } from 'react';
+
+import { ProfileContext } from '../context/ProfileContext';
+import { userApi } from '../services/api';
+
+type Profile = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
+export function UpdateProfileList(profileList: Profile[]) {
+  const { getProfileList } = useContext(ProfileContext);
+  
+  useEffect(() => {
+    if (profileList.length === 0) {
+      Promise.all([userApi.get('users')])
+        .then(([{ data }]) => {
+          getProfileList(data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }, [profileList, getProfileList]);
+}
