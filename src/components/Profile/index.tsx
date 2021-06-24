0fdbfc367b493b44651706de87efe8a7619d01d5
FelipePDS <felipepdasilva66@gmail.com>
2021-06-24 19:03:04 -0300

    add toggleSelectedProfileId from ProfileContext

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { ProfileContext } from '../../context/ProfileContext';
 
 import { Container, ProfileAvatar } from './styles';
 
 export type Props = {
+  profileId: string;
   profileName: string;
   profileAvatar: string;
 }
 
 const ProfileControl: React.FC<Props> = ({
+  profileId,
   profileName,
   profileAvatar
 }) => {
+
+  const { toggleSelectedProfileId } = useContext(ProfileContext);
+  
   return (
     <Container>
-      <Link to="/browse">
+      <Link 
+        onClick={() => toggleSelectedProfileId(profileId)}
+        to="/browse"
+      >
         <ProfileAvatar 
           className="profileAvatar" 
 
+          profileId={profileId}
           profileName={profileName}
           profileAvatar={profileAvatar}
         />
