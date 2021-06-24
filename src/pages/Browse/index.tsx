7b32135c14ab6244ead579aa81cc050e3676e380
FelipePDS <felipepdasilva66@gmail.com>
2021-06-24 18:58:31 -0300

    add Browse page

--- src/pages/Browse/index.tsx
+++ src/pages/Browse/index.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { ProfileContext } from '../../context/ProfileContext';
+import { userApi } from '../../services/api';
+import { UpdateProfileList } from '../../utils/profileUtil';
+
+import { Container } from './styles';
+
+import MenuTop from '../../components/MenuTop';
 
 const Browse: React.FC = () => {
+  const history = useHistory();
+
+  const { 
+    profileList,
+    wasCaughtProfile,
+    getProfile
+  } = useContext(ProfileContext);
+
+  UpdateProfileList(profileList);
+
+  useEffect(() => {
+    if (!wasCaughtProfile) {
+      if (localStorage.SelectedProfileId !== undefined) {
+
+        Promise.all([
+          userApi.get('users', {
+            params: {
+              id: localStorage.SelectedProfileId
+            }
+          })
+        ])
+          .then(([{ data }]) => {
+            getProfile(data[0]);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+          
+      } else {
+        history.push('/');
+      }
+    }
+  }, [history, wasCaughtProfile, getProfile]);
+
   return (
-    <div>
-      Filmes
-    </div>
+    <Container>
+      <MenuTop />
+    </Container>
   );
 };
 
