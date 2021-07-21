9d0544507a8790ec2f3f633258fdc067957d162e
FelipePDS <felipepdasilva66@gmail.com>
2021-07-20 22:36:01 -0300

    add movieApi

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const userApi = axios.create({
   baseURL: 'http://localhost:3333/'
 });
+
+export const movieApi = axios.create({
+  baseURL: 'https://api.themoviedb.org/3'
+});
