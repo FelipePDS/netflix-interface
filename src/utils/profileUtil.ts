d3a0f128445e8e04c6ce1e5a55c5927e67626955
FelipePDS <felipepdasilva66@gmail.com>
2021-08-01 04:06:19 -0300

    remove method parameter

--- src/utils/profileUtil.ts
+++ src/utils/profileUtil.ts
@@ -3,22 +3,19 @@ import { useContext } from 'react';
 import { ProfileContext } from '../context/ProfileContext';
 import { userApi } from '../services/api';
 
-type Profile = {
-  id: string;
-  name: string;
-  avatar: string;
-};
+export function LoadProfileList(): void {
+  const { 
+    profileList,
+    getProfileList 
+  } = useContext(ProfileContext);
 
-export function UpdateProfileList(profileList: Profile[]) {
-  const { getProfileList } = useContext(ProfileContext);
-  
   if (profileList.length === 0) {
-    Promise.all([userApi.get('users')])
-      .then(([{ data }]) => {
+    userApi.get('users')
+      .then(({ data }) => {
         getProfileList(data);
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
       });
   }
 }
