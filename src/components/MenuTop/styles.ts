1468b37a2c6cce030b67704a4cdaefdf17124ffd
FelipePDS <felipepdasilva66@gmail.com>
2021-08-06 19:11:01 -0300

    add new styles

--- src/components/MenuTop/styles.ts
+++ src/components/MenuTop/styles.ts
@@ -11,6 +11,8 @@ export const Container = styled.div`
   height: 4.25rem;
   width: 100%;
 
+  z-index: 3;
+
   background-image: linear-gradient(to bottom,rgba(0,0,0,.7) 10%,rgba(0,0,0,0));
   background-image: -webkit-gradient(linear,left top,left bottom,color-stop(10%,rgba(0,0,0,.7)),color-stop(10%,rgba(0,0,0,0)));
   background-image: -webkit-linear-gradient(top,rgba(0,0,0,.7) 10%,rgba(0,0,0,0));
