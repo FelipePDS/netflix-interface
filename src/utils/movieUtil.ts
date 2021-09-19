33ee2419e9ddab9e9f0763e21933cc26cf44d406
FelipePDS <felipepdasilva66@gmail.com>
2021-09-18 21:52:33 -0300

    refactore all methods

--- src/utils/movieUtil.ts
+++ src/utils/movieUtil.ts
@@ -1,97 +1,122 @@
 import { AxiosResponse } from 'axios';
 
 import { 
-    GenreProps,
-    FormatedMovieProps,
-    SectionMovieProps
+	GenreProps,
+	FormatedMovieProps,
+	SectionMovieProps
 } from '../context/MovieContext';
 
 import { movieApi } from '../services/api';
 
 type MovieApiRoutePathsProps = {
-    name: string;
-    routePath: string;
+	name: string;
+	routePath: string;
+  isPoster?: boolean;
 };
 
-function formatMovieList(
-    movieList: FormatedMovieProps[],
-    genreList: GenreProps[]
-): FormatedMovieProps[] {
-    return movieList.map(movie => {
-        const first_air_year = movie.first_air_date?.split('-')[0];
-        const rating = `${movie.vote_average * 10}%`;
+function requestUrlsMovieApi(
+	movieApiRoutePaths: MovieApiRoutePathsProps[]
+): Promise<AxiosResponse<any>>[] {
+	const currentLanguage = navigator.language;
+	const apiKey = process.env.REACT_APP_API_KEY;
+  
+	const movieRouteParams = `
+		language=${currentLanguage}&api_key=${apiKey}
+	`;
 
-        const imagePathBase = 'https://image.tmdb.org/t/p/original';
-        const backdrop_path_full = imagePathBase + movie.backdrop_path;
-        const poster_path_full = imagePathBase + movie.poster_path;
+	const requests = movieApiRoutePaths.map(({ routePath }) => {
+		const url = routePath.concat(movieRouteParams);
 
-        const seasons = `
-            ${movie.number_of_seasons} Temporada${
-                movie.number_of_seasons === 1 ? '' : 's'
-            }
-        `;
+		return movieApi.get(url);
+	});
 
-        const genres = movie.genre_ids.map(genreId => (
-            genreList.find(({ id, name }) => (
-                id === Number(genreId)
-                    && {
-                        id,
-                        name
-                    }
-            ))
-        ));
-
-        return {
-            ...movie,
-            first_air_year,
-            rating,
-            backdrop_path_full,
-            poster_path_full,
-            seasons,
-            genres
-        }
-    });
+	return requests;
 }
 
-function requestUrlsMovieApi(
-    movieApiRoutePaths: MovieApiRoutePathsProps[]
-): Promise<AxiosResponse<any>>[] {
-    const currentLanguage = navigator.language;
-    const apiKey = process.env.REACT_APP_API_KEY;
-  
-    const movieBaseRoute = `
-        language=${currentLanguage}&api_key=${apiKey}
-    `;
+async function getGenreList(): Promise<GenreProps[]> {
+	const currentLanguage = navigator.language;
+	const apiKey = process.env.REACT_APP_API_KEY;
 
-    const requests = movieApiRoutePaths.map(({ routePath }) => {
-        const url = routePath.concat(movieBaseRoute);
+	const movieGenresResponses = await movieApi.get(
+		`/genre/list?language=${currentLanguage}&api_key=${apiKey}`
+	);
+	const movieGenresList: GenreProps[] = movieGenresResponses.data.genres;
 
-        return movieApi.get(url);
-    });
+	const tvGenresResponses = await movieApi.get(
+		`/genre/tv/list?language=${currentLanguage}&api_key=${apiKey}`
+	);
+	const tvGenresList: GenreProps[] = tvGenresResponses.data.genres;
 
-    return requests;
+	const genreList = movieGenresList.concat(tvGenresList);
+
+	return genreList;
+}
+
+function formatMovieList(
+	movieList: FormatedMovieProps[], genreList: GenreProps[]
+): FormatedMovieProps[] {
+	return movieList.map(movie => {
+		const first_air_year = movie.first_air_date?.split('-')[0];
+		const rating = `${movie.vote_average * 10}%`;
+
+		const images_base_url = `${process.env.REACT_APP_IMAGES_URL_BASE}`;
+
+		const imageSizeOriginal = `${process.env.REACT_APP_IMAGE_SIZE_ORIGINAL}`;
+		const imageSizeW1280 = `${process.env.REACT_APP_IMAGE_SIZE_W1280}`;
+
+		const backdrop_path_original_url = images_base_url + imageSizeOriginal + movie.backdrop_path;
+		const backdrop_path_w1280_url = images_base_url + imageSizeW1280 + movie.backdrop_path;
+		
+		const poster_path_original_url = images_base_url + imageSizeOriginal + movie.poster_path;
+		const poster_path_w1280_url = images_base_url + imageSizeW1280 + movie.poster_path;
+
+		const seasons = `
+			${movie.number_of_seasons} Temporada${
+				movie.number_of_seasons === 1 ? '' : 's'
+			}
+		`;
+
+		const genres = movie.genre_ids.map(genreId => (
+			genreList.find((genre) => (
+				genre.id === Number(genreId) && genre
+			))
+		));
+
+		return {
+			...movie,
+			first_air_year,
+			rating,
+			images_base_url,
+			backdrop_path_original_url,
+			backdrop_path_w1280_url,
+			poster_path_original_url,
+			poster_path_w1280_url,
+			seasons,
+			genres
+		}
+	});
 }
 
 function raffleFeaturedMovieIndex(
-    sectionMoviesResponses: SectionMovieProps[],
-    sectionFeaturedMovieIndex: number
+	sectionFeaturedMovie: SectionMovieProps,
 ): number {
-    let randomFeaturedMovieIndex: number;
+	let randomFeaturedMovieIndex: number;
 
-    do {
-        randomFeaturedMovieIndex = Math.floor(
-            Math.random() * sectionMoviesResponses[sectionFeaturedMovieIndex].movies.length
-        );
-    } while (
-        sectionMoviesResponses[sectionFeaturedMovieIndex]
-            .movies[randomFeaturedMovieIndex].backdrop_path === null
-    );
+	do {
+		randomFeaturedMovieIndex = Math.floor(
+			Math.random() * sectionFeaturedMovie.movies.length
+		);
+	} while (
+		sectionFeaturedMovie.movies[randomFeaturedMovieIndex].backdrop_path === null
+			|| sectionFeaturedMovie.movies[randomFeaturedMovieIndex].overview === ''
+	);
 
-    return randomFeaturedMovieIndex;
+	return randomFeaturedMovieIndex;
 }
 
 export { 
-    formatMovieList,
-    requestUrlsMovieApi,
-    raffleFeaturedMovieIndex
+	requestUrlsMovieApi,
+	getGenreList,
+	formatMovieList,
+	raffleFeaturedMovieIndex
 }
